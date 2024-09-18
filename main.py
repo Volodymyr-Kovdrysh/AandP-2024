abad849dced8e45027a577c8bee8fe59e1a76d63
VVKo <v.v.kovdrysh@gmail.com>
2024-09-18 14:53:49 +0300

    fix user_action
    Signed-off-by: VVKo <v.v.kovdrysh@gmail.com>

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ todos = []
 
 while True:
     user_action = input('Type add, show or exit: ')
+    user_action = user_action.strip()
+
     match user_action:
         case 'add':
             todo = input('Enter a todo: ')
