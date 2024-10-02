1db7c044979e99c9bb48e99461a5009b52427b98
VVKo <v.v.kovdrysh@gmail.com>
2024-10-02 16:02:07 +0300

    Кінець лекції №6
    Signed-off-by: VVKo <v.v.kovdrysh@gmail.com>

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-
-def get_todos(filepath):
-    with open(filepath, 'r') as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
-
-def write_todos(filepath, todos_arg):
-    with open(filepath, 'w') as file_local:
-        file_local.writelines(todos_arg)
-
+from functions import get_todos, write_todos
 
 while True:
     user_action = input('Type add, show, edit, complete or exit: ')
@@ -16,14 +7,14 @@ while True:
 
     if user_action.startswith('add'):
         todo = user_action[4:] + '\n'
-        todos = get_todos('files/todos.txt')
+        todos = get_todos()
 
         todos.append(todo)
 
-        write_todos('files/todos.txt', todos)
+        write_todos(filepath='files/todos.txt', todos_arg=todos)
 
     elif user_action.startswith('show'):
-        todos = get_todos('files/todos.txt')
+        todos = get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -33,12 +24,12 @@ while True:
         try:
             number = int(input('Number of the todo to edit: '))
             number = number - 1
-            todos = get_todos('files/todos.txt')
+            todos = get_todos()
 
             new_todo = input('Enter new todo: ')
             todos[number] = new_todo + '\n'
 
-            write_todos('files/todos.txt', todos)
+            write_todos(todos)
 
         except ValueError:
             print('Your number is not valid')
@@ -47,7 +38,7 @@ while True:
     elif user_action.startswith('complete'):
         try:
             number = int(input('Number of the todo to complete: '))
-            todos = get_todos('files/todos.txt')
+            todos = get_todos()
 
             # todo подумати над кращою реалізацію...
 
@@ -59,7 +50,7 @@ while True:
             todo_to_remove = todos[index].strip('\n')
             todos.pop(number - 1)
 
-            write_todos('files/todos.txt', todos)
+            write_todos(todos)
 
             message = f"Todo {todo_to_remove} was removed from the list!"
             print(message)
