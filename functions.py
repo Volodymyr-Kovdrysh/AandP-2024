391df02549a291e9915a5fe7b13f9e7dfc5cc8dd
VVKo <v.v.kovdrysh@gmail.com>
2024-10-09 16:03:27 +0300

    Кінець Лекції №7
    Signed-off-by: VVKo <v.v.kovdrysh@gmail.com>

--- functions.py
+++ functions.py
@@ -1,12 +1,18 @@
-def get_todos(filepath='files/todos.txt'):
+FILEPATH = 'files/todos.txt'
+
+def get_todos(filepath=FILEPATH):
     """ Read a txt file and return list of to-do items"""
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
-def write_todos(todos_arg, filepath='files/todos.txt' ):
+def write_todos(todos_arg, filepath=FILEPATH ):
     """
     Write the to-do list in the text file
     """
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_arg)
+
+
+if __name__ == '__main__':
+    print(get_todos())
